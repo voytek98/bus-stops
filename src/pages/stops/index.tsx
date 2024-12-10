096dbeb6a167824b1993b4cd893ab5ac092f6aec
Wojciech Florczak <florczak@wojciech.dev>
2024-12-10 14:56:28 +0100

    Update StopsPage (#9)
    create useDebounce hook,
    update StopsPage

--- src/pages/stops/index.tsx
+++ src/pages/stops/index.tsx
@@ -1,3 +1,60 @@
-export const StopsPage = () => {
-  return <div>Stops page</div>;
+import { useState } from 'react';
+import { useBusStore } from '@app/stores';
+import { PanelWithList } from '@app/components';
+import { useDebounce } from '@app/hooks';
+import arrowBottom from '@app/assets/icons/arrow-bottom-rec.svg';
+
+const StopsPage = () => {
+  const { stopList } = useBusStore();
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [debouncedSearchTerm] = useDebounce(searchTerm, 300);
+  const rotateDeg = sortDirection === 'asc' ? '180deg' : '0deg';
+  const sortedList = stopList.sort((a, b) =>
+    sortDirection === 'asc'
+      ? a.stop.localeCompare(b.stop)
+      : b.stop.localeCompare(a.stop)
+  );
+  const filteredList = sortedList.filter((stop) =>
+    stop.stop.toLowerCase().includes(debouncedSearchTerm.toLowerCase())
+  );
+  const handleClick = () => {
+    setSortDirection((prev) => (prev === 'asc' ? 'desc' : 'asc'));
+  };
+
+  return (
+    <div className="row g-0 bg-white rounded-2">
+      <div className="col-3 p-2">
+        <div className="input-group input-group-sm">
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-sm"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="col-12">
+        <PanelWithList
+          header={
+            <>
+              <span className="fs-6">Bus Stops</span>
+              <img
+                role="button"
+                style={{ rotate: rotateDeg }}
+                src={arrowBottom}
+                onClick={handleClick}
+              />
+            </>
+          }
+          list={filteredList.map((stop) => stop.stop)}
+        />
+      </div>
+    </div>
+  );
 };
+
+export { StopsPage };
