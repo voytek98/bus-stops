3b28924ac7e8b49960f73234e17f9de5710ca59c
Wojciech Florczak <florczak@wojciech.dev>
2024-12-10 13:46:30 +0100

    Create global styles config (#4)
    update config to use sass,
    import bootstrap styles,
    add global styles,
    add font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Router from './router';
+import './styles.scss';
 
 const root = document.getElementById("root");
 
