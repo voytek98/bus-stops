c4fc5d10de085ea93ec6cc208e68dbcd48ba41fe
voytek98 <florczak@wojciech.dev>
2024-12-10 13:45:13 +0100

    update config to use sass, import bootstrap styles, add global styles, add font

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { NodePackageImporter } from 'sass';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        importers: [new NodePackageImporter()],
+      },
+    },
+  },
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, 'src'),
